cf4969db6e7b26aaab5214ead1143986d0003dde
seyhaheng2 <seyhaheng2@gmail.com>
2014-05-13 14:04:41 +0700

    hery hery

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,14 +2,24 @@ class SiteController < ApplicationController
   def index
     @products = Product.paginate(page: params[:page],per_page: 12)
   end
+
   def blog
   end
 
   def tutorails
   end
+
   def show
     @product = Product.find(params[:product_id])
+    def next
+      products.next(self.id).first
+    end
+
+    def previous
+      products.previous(self.id).first
+    end
   end
+
   def topics
   end
 
